1450718b091914222edd3330adc93f7d654d686d
CodeWizard <132087862+CodeWizard100@users.noreply.github.com>
2024-08-18 13:25:57 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,6 @@
-#!/bin/sh
 
-# Example installation script
-set -e
-
-# Build the image
 aports/scripts/mkimage.sh --tag edge \
   --outdir ~/iso \
   --arch x86_64 \
   --repository https://dl-cdn.alpinelinux.org/alpine/edge/main \
   --profile standard
-
-# Sign the image
-gpg --detach-sign --armor ~/iso/your-image.iso
