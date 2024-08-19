507a7de6565fad32197b01d10bf6157a134e1e6b
CodeWizard <132087862+CodeWizard100@users.noreply.github.com>
2024-08-19 20:25:45 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,68 +1,74 @@
-# config
+#!/bin/bash
 
+# Define variables with correct syntax
 arch=${1:-amd64}
-mirror=${2:-”http://archive.ubuntu.com/ubuntu/”}
-release=${3;-xenial}sudo apt-get install debootstrapsudo debootstrap --arch=${arch} ${release} chroot ${mirror}
+mirror=${2:-"http://archive.ubuntu.com/ubuntu/"}
+release=${3:-xenial}
 
-# copy sources.list
+# Install necessary package and run debootstrap
+sudo apt-get install -y debootstrap
+sudo debootstrap --arch=${arch} ${release} chroot ${mirror}
 
+# Copy sources.list
 sudo cp -v sources.${release}.list chroot/etc/apt/sources.list
 
-# mount filesystem to chroot
-
+# Mount filesystem to chroot
 sudo mount --rbind /sys chroot/sys
 sudo mount --rbind /dev chroot/dev
 sudo mount -t proc none chroot/proc
 
-# run chroot
-
-sudo  chroot chroot <<EOF
-# linking /sbin/initctl to /bin/true
+# Run chroot commands
+sudo chroot chroot <<EOF
+# Linking /sbin/initctl to /bin/true
 ln -s /bin/true /sbin/initctl
+
 # Upgrading the packages
-apt-get --y upgrade
+apt-get -y upgrade
+
 # Installing core packages
-apt-get -qq -y --purge install ubuntu-standard-casper lupin-casper \laptop-detect os-prober linux-generic
+apt-get -qq -y --purge install ubuntu-standard casper lupin-casper laptop-detect os-prober linux-generic
+
 # Installing base packages
-apt-get  -qq -y install xorg xinit sddm
+apt-get -qq -y install xorg xinit sddm
+
 # Installing LXQt components
-apt-get -qq -y install lxqt openbox# Cleaning up the ChRoot environment
+apt-get -qq -y install lxqt openbox
+
+# Cleaning up the ChRoot environment
 rm /sbin/initctl
 apt-get -qq clean
-rm -rf /tmp/*dpkg-divert --rename --remove /sbin/initctl
+rm -rf /tmp/*
+dpkg-divert --rename --remove /sbin/initctl
 exit
 EOF
 
-# copy required files
-
+# Copy required files
 tar xf image-amd64.tar.lzma
 sudo \cp --verbose -rf chroot/boot/vmlinuz-**-generic image/casper/vmlinuz
 sudo \cp --verbose -rf chroot/boot/initrd.img-**-generic image/casper/initrd.lz
 
 # Create manifest
-
 sudo chroot chroot dpkg-query -W --showformat='${Package} ${Version}\n' | sudo tee image/casper/filesystem.manifest
 sudo cp -v image/casper/filesystem.manifest image/casper/filesystem.manifest-desktop
+
 REMOVE='ubiquity ubiquity-frontend-gtk ubiquity-frontend-kde casper lupin-casper live-initramfs user-setup discover1 xresprobe os-prober libdebian-installer4'
-for i in $REMOVE
-do
-        sudo sed -i "/${i}/d" image/casper/filesystem.manifest-desktop
+for i in $REMOVE; do
+    sudo sed -i "/${i}/d" image/casper/filesystem.manifest-desktop
 done
 
-# Compress
-
+# Compress the filesystem
 sudo mksquashfs chroot image/casper/filesystem.squashfs -noappend -no-progress
 
-# Create iso
-
+# Create ISO image
 IMAGE_NAME=${IMAGE_NAME:-"CUSTOM ${release} $(date -u +%Y%m%d) - ${arch}"}
-sudo apt-get install genisoimage
-sudo genisoimage -r -V “$IMAGE_NAME” -cache-inodes -J  -l \
-    -allow-limited-size -udf \
-    -b isolinux/isolinux.bin -c isolinux/boot.cat \ 
-    -no-emul-boot -boot-load-size 4 -boot-info-table 
-    -p “${DEBFULLNAME:- $USER} <${DEBMAIL:-on host $(hostname        --fqdn)}>” \
-    -A “$IMAGE_NAME” \
-    -o ../$ISOFILE .
+ISOFILE="custom-ubuntu.iso"
+sudo apt-get install -y genisoimage
 
-    
+# Generate the ISO
+sudo genisoimage -r -V "$IMAGE_NAME" -cache-inodes -J -l \
+    -allow-limited-size -udf \
+    -b isolinux/isolinux.bin -c isolinux/boot.cat \
+    -no-emul-boot -boot-load-size 4 -boot-info-table \
+    -p "${DEBFULLNAME:-$USER} <${DEBMAIL:-on host $(hostname --fqdn)}>" \
+    -A "$IMAGE_NAME" \
+    -o ../$ISOFILE .
